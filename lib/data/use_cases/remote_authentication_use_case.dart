4c0294d06355615b7fe597633e17466a42cdf2df
robsonsilv4 <robsonsilv410@gmail.com>
2020-10-09 13:58:34 -0300

    fix: move api details to data

--- lib/data/use_cases/remote_authentication_use_case.dart
+++ lib/data/use_cases/remote_authentication_use_case.dart
@@ -3,6 +3,24 @@ import 'package:meta/meta.dart';
 import '../../domain/use_cases/use_cases.dart';
 import '../http/http.dart';
 
+class RemoteAuthenticationParams {
+  final String email;
+  final String password;
+
+  RemoteAuthenticationParams({
+    @required this.email,
+    @required this.password,
+  });
+
+  factory RemoteAuthenticationParams.fromDomain(AuthenticationParams params) =>
+      RemoteAuthenticationParams(
+        email: params.email,
+        password: params.password,
+      );
+
+  Map toJson() => {'email': email, 'password': password};
+}
+
 class RemoteAuthenticationUseCase {
   final HttpClient httpClient;
   final String url;
@@ -13,10 +31,11 @@ class RemoteAuthenticationUseCase {
   });
 
   Future<void> auth(AuthenticationParams params) async {
+    final body = RemoteAuthenticationParams.fromDomain(params).toJson();
     await httpClient.request(
       url: url,
       method: 'post',
-      body: params.toJson(),
+      body: body,
     );
   }
 }
