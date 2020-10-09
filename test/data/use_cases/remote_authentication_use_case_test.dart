7c3927db94c29cf737b44297591fb9a73ddb249d
robsonsilv4 <robsonsilv410@gmail.com>
2020-10-09 11:21:39 -0300

    test: authentication calls http with correct body

--- test/data/use_cases/remote_authentication_use_case_test.dart
+++ test/data/use_cases/remote_authentication_use_case_test.dart
@@ -1,4 +1,5 @@
 import 'package:faker/faker.dart';
+import 'package:flutter_manguinho/domain/use_cases/use_cases.dart';
 import 'package:meta/meta.dart';
 import 'package:mockito/mockito.dart';
 import 'package:test/test.dart';
@@ -7,6 +8,7 @@ abstract class HttpClient {
   Future<void> request({
     @required String url,
     @required String method,
+    Map body,
   });
 }
 
@@ -21,8 +23,9 @@ class RemoteAuthentication {
     @required this.url,
   });
 
-  Future<void> auth() async {
-    await httpClient.request(url: url, method: 'post');
+  Future<void> auth(AuthenticationParams params) async {
+    final body = {'email': params.email, 'password': params.password};
+    await httpClient.request(url: url, method: 'post', body: body);
   }
 }
 
@@ -38,11 +41,19 @@ void main() {
   });
 
   test('should call HttpClient with correct values', () async {
-    await sut.auth();
+    final params = AuthenticationParams(
+      email: faker.internet.email(),
+      password: faker.internet.password(),
+    );
+    await sut.auth(params);
 
     verify(httpClient.request(
       url: url,
       method: 'post',
+      body: {
+        'email': params.email,
+        'password': params.password,
+      },
     ));
   });
 }
