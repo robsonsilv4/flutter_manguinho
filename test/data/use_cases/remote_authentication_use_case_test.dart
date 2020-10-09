870cfc114f02b6774a3d6259ac2ba18a87453a7c
robsonsilv4 <robsonsilv410@gmail.com>
2020-10-09 15:11:17 -0300

    test: throws unexpected error on 500

--- test/data/use_cases/remote_authentication_use_case_test.dart
+++ test/data/use_cases/remote_authentication_use_case_test.dart
@@ -64,4 +64,18 @@ void main() {
 
     expect(future, throwsA(DomainError.unexpected));
   });
+
+  test('should throw UnexpectedError if HttpClient returns 404', () async {
+    when(
+      httpClient.request(
+        url: anyNamed('url'),
+        method: anyNamed('method'),
+        body: anyNamed('body'),
+      ),
+    ).thenThrow(HttpError.serverError);
+
+    final future = sut.auth(params);
+
+    expect(future, throwsA(DomainError.unexpected));
+  });
 }
